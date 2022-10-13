5421cdbfe17e055e3bb4289f7fec4e9a629ffa57
iago-moreira <iago.moreira@ibm.com>
2022-10-13 20:19:32 -0300

    finish plotter module

--- deposition/plotter/plotter.py
+++ deposition/plotter/plotter.py
@@ -1,19 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class Plotter ():
     """
     Plotter - A class that receives a binary matrix M (n x m)
     and provides plotting optios to save it as images. 
     """
 
-    def __init__(self,num_lines:int,num_columns:int) -> bool:
+    def __init__(self,num_lines:int,num_columns:int) -> None:
         try:
-            self.__num_lines = int(num_lines)
+            self.__num_rows = int(num_lines)
             self.__num_columns = int(num_columns)
         except ValueError :
             raise (ValueError("num_lines and num_columns should be integers"))
-        return True
+    
 
     def __is_valid_item (self, num:int) -> bool:
         """
@@ -22,28 +23,44 @@ class Plotter ():
         return True if (num == 1 or num ==0 ) else False
 
 
+
+    def __invert_colors(self):
+        for row in range(self.__num_rows):
+            for column in range(self.__num_columns):
+                self.__matrix[row][column] = 1 if self.__matrix[row][column]==0 else 0 
+
+
     def set_matrix(self, A) -> bool:
+        """
+        Define the matrix that will be printed.
+        Checks for dimentional consistency and 
+        """
+        rows = len(A)
+        columns = len(A[0])
+
+        if (rows != self.__num_rows or columns != self.__num_columns):
+            raise IndexError(f'A must be a matrix {self.__num_rows} by {self.__num_columns}')
+
         for row in A:
             for item in row:
                 if self.__is_valid_item(item):
                     pass
                 else:
-                     raise ValueError(f'Matrix item should be 1 or 0, founded {item}')
+                     raise ValueError(f'Matrix item should be 1 or 0, founded {type(item)}')
+        
         self.__matrix = A 
         return True
 
-    def plot(self):
-        matrix = np.identity(10)
-        matrix[0][0] = 0
-        inverted_matrix = matrix[::-1]
-        plt.matshow(inverted_matrix,cmap='gray', vmin=1, vmax=0)
-        plt.axis(False)
-        plt.show()
-        print(inverted_matrix)
-
-
-if __name__ == "__main__":
-    pltt = Plotter(3,3)
-    matrix = np.identity(100, int)
-    matrix[99][99] = 2
-    print(pltt.set_matrix(matrix))
+    def plot(self, fig_name:str) -> bool:
+        """
+        Prints the matrix using matshow
+        """
+        try:
+            self.__invert_colors()
+            inverted_matrix = self.__matrix[::-1]
+            plt.matshow(inverted_matrix,cmap='gist_yarg_r', vmin=0, vmax=1)
+            plt.axis(False)
+            plt.savefig(fig_name + '.png', format='png')
+            return True
+        except Exception as e:
+            raise (e)
--- deposition/plotter/test_plotter.py
+++ deposition/plotter/test_plotter.py
@@ -1,6 +1,8 @@
 from plotter import Plotter
 import numpy as np
 import pytest
+import os
+import time
 
 def test_plotter_init_success ():
     p = Plotter(10,10)
@@ -14,6 +16,24 @@ def test_matrix_setter ():
     matrix = np.identity(10)
     results = plt_tst.set_matrix(matrix)
     assert results == True
+
+def test_matrix_setter_fail_len ():
+    plt_tst = Plotter(10,9)
+    matrix = np.identity(10)
+    with pytest.raises(IndexError):
+        results = plt_tst.set_matrix(matrix)
+
+def test_plotting():
+    pltt = Plotter(10,10)
+    matrix = np.identity(10, int)
+    pltt.set_matrix(matrix)
+    path = os.getcwd()
+    fig_name = str(time.time())
+    path = os.path.join(path,fig_name)
+    pltt.plot(path)
+    fig_exists = os.path.isfile(path + '.png')
+    os.remove(path + '.png')
+    assert fig_exists == True
     
 def test_matrix_setter_fail ():
     with pytest.raises(ValueError):
